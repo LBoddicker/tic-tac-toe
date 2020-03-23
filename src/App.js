35ec64721fbbc32f5a652da855fc0d3fa11e4929
Lake Boddicker <lakeboddicker@gmail.com>
2020-03-23 11:41:26 -0500

    added squares

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import GameSquare from './GameSquare/GameSquare'
 
 function App() {
   return (
     <div className="App">
+        <GameSquare/>
     </div>
   );
 }
