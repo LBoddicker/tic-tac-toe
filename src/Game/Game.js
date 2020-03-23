bf6a0893abf5931787ce52a468aa807a298a6576
Lake Boddicker <lakeboddicker@gmail.com>
2020-03-23 15:18:19 -0500

    created game component

---
+++ src/Game/Game.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import './Game.css'
+import GameBoard from '../GameBoard/GameBoard'
+
+class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {  cur_state : [null,null,null,null,null,null,null,null,null],
+                        xIsNext: true,
+                        };
+    }
+
+    squareClickHandler = (squareNum) => {
+        var tempBoard = this.state.cur_state.slice();
+        tempBoard[squareNum] = this.state.xIsNext? 'X' : 'O';
+        this.setState({
+            cur_state : tempBoard,
+            xIsNext: !this.state.xIsNext,
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <GameBoard boardState={this.state.cur_state} callBack={this.squareClickHandler}/>
+            </div>
+        );
+    }
+}
+
+export default Game
