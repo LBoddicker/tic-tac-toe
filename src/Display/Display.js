6210922f08441cd34b5a8785ea7538b001b8b233
Lake Work <LakeWork@Lakes-MacBook-Pro.local>
2020-04-30 22:16:29 -0500

    switch to material UI

--- src/Display/Display.js
+++ src/Display/Display.js
@@ -1,12 +1,54 @@
 import React from 'react'
 import './Display.css'
+import GameBoard from '../GameBoard/GameBoard'
+
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+const useStyles = makeStyles((theme) => ({
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+    },
+  }));
 
 const Display = (props) => {
+    const classes = useStyles();
+
+
 
     return (
-        <div>
+        <Container component="main" maxWidth="md">
+            <CssBaseline />
+            <Typography component="h1" variant="h3" align="center" color="textPrimary" gutterBottom>
+                TIC-TAC-TOE
+            </Typography>
 
-        </div>
+            <Typography variant="h5" align="center" color="textSecondary" paragraph>
+                {props.playerTurn}
+            </Typography>
+
+            <Grid container spacing={2} justify="center" direction="column" alignItems="center">
+
+                <GameBoard boardState={props.boardState} callBack={props.callBack}/>
+
+                <Button
+                    type="submit"
+                    variant="contained"
+                    color="primary"
+                    className={classes.submit}
+                    onClick={props.resetCallBack}
+                >
+                    {props.buttonMessage}
+                </Button>
+
+            </Grid>
+
+            
+        </Container>
     );
 }
 
--- src/GameSquare/GameSquare.js
+++ src/GameSquare/GameSquare.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import './GameSquare.css'
 
+import Button from '@material-ui/core/Button';
+
 function GameSquare(props) {
     return (
         <div>
-            <button className="game_button" onClick={props.clickFunc}>
+            <Button variant="outlined" color="primary" className="game_button" onClick={props.clickFunc}>
                 {props.value}
-            </button>
+            </Button>
         </div>
     );
 }
