132b9bab63b281caf48d8a4f30784a56e17699dd
Lake Work <LakeWork@Lakes-MacBook-Pro.local>
2020-04-30 22:25:20 -0500

    update font size in square

--- src/GameSquare/GameSquare.js
+++ src/GameSquare/GameSquare.js
@@ -6,7 +6,13 @@ import Button from '@material-ui/core/Button';
 function GameSquare(props) {
     return (
         <div>
-            <Button variant="outlined" color="primary" className="game_button" onClick={props.clickFunc}>
+            <Button
+                variant="outlined"
+                color="primary"
+                className="game_button"
+                onClick={props.clickFunc}
+                style={{ fontSize: '30px' }}
+            >
                 {props.value}
             </Button>
         </div>
